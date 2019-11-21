d4e0ca4570a8285e3ee739436468e023e09b1dd6
Don-Droid <raldon.baxter@mymona.uwi.edu>
2019-11-21 15:55:53 -0500

    Excercise 5 completed

--- world.js
+++ world.js
@@ -1,13 +1,37 @@
 
 const lookupButton = document.getElementById("lookup"); //Get the Button object and save to a variable
+const lookupButton2 = document.getElementById("lookup2"); //Get the Button object and save to a variable
 lookupButton.onclick = RespondClick; //function call to RespondClick when button is clicked
+lookupButton2.onclick = RespondClick2; //function call to RespondClick when button is clicked
 let sanitizedInput = ""; //declaration of sanitized input variable 
+let input2 = "";
 
 function encodeHTML(s) {      //helper function to sanitize user input (remove special character)
     return s.replace(/[^a-zA-Z '-]/g,'');
 }
     
 function RespondClick() {  // This function create new XMLHttpRequest object
+    input2 = "&context=country";
+    const searchInput = document.getElementById("search").value; //Save search Input as a variable
+    sanitizedInput = encodeHTML(searchInput); // function call to sanitize function
+    const xhr = new XMLHttpRequest (); 
+    const url = '/world.php?country='; //Url to server
+    xhr.onreadystatechange = dosomething;
+    function dosomething (){
+        if (xhr.readyState === 4){
+            if(xhr.status === 200){                
+                var response = xhr.responseText;
+                document.getElementById("result").innerHTML=response;
+            }else{
+                console.log("File not found");
+        }
+    }
+    }
+    xhr.open('GET',url+sanitizedInput+input2,true);
+    xhr.send(); 
+}
+function RespondClick2() {  // This function create new XMLHttpRequest object
+    input2 = "&context=cities"
     const searchInput = document.getElementById("search").value; //Save search Input as a variable
     sanitizedInput = encodeHTML(searchInput); // function call to sanitize function
     const xhr = new XMLHttpRequest (); 
@@ -23,6 +47,6 @@ function RespondClick() {  // This function create new XMLHttpRequest object
         }
     }
 }
-xhr.open('GET',url+sanitizedInput,true);
+xhr.open('GET',url+sanitizedInput+input2,true);
 xhr.send();     
 } 
--- world.php
+++ world.php
@@ -13,14 +13,15 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
+<?php if ($_GET['context'] == "country"):?>
 
-<table align="center" border="1px" style= "width:600px; line-height:40px;">
-    <tr>
-        <th>Name</th>
-        <th>Continent</th>
-        <th>Independence Year</th>
-        <th>Head of State</th>
-    </tr>
+    <table id="table" align="center" border="1px" style= "width:600px; line-height:40px;">
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
 <?php foreach ($results as $row): ?>
     <tr>
   <td><?= $row['name']; ?></td>
@@ -30,4 +31,21 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   </tr>
 <?php endforeach; ?>
 </table>
+<?php elseif($_GET['context'] == "cities"):?>
+<?php   $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$input%'");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+    <table align="center" border="1px" style= "width:600px; line-height:40px;">
+        <tr>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+    <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['district']; ?></td>
+      <td><?= $row['population']; ?></td>
+  </tr>
+<?php endforeach; ?>  
+<?php endif; ?>
 
